3769ab18433e911a8abee944aca108a44b32330f
Francis Avila <favila@breezeehr.com>
2014-07-25 18:29:14 -0500

    Bump cljs to 2277.

--- project.clj
+++ project.clj
@@ -1,20 +1,21 @@
 (defproject favila/utfate "0.1.0-SNAPSHOT"
   :description "Fast javascript utf-8 encoder and decoder."
   :url "http://github.com/favila/utfate"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2234"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2277"]]
   :source-paths ["src" "test"]
   :profiles
   {:dev
    {:plugins [[lein-cljsbuild "1.0.3"]]
     :hooks [leiningen.cljsbuild]
     :cljsbuild
-    {:test-commands {"test" ["/Users/favila/js-vms/v8/d8" "target/test/utfate_test.js"]}
+    {:test-commands {"test" ["d8" "target/test/utfate_test.js"]}
      :builds
      {"test"
       {:source-paths ["src" "test"]
        :compiler {:output-to "target/test/utfate_test.js"
-                  :optimizations :whitespace
+                  :optimizations :advanced
+                  :pseudo-names true
                   :static-fns true}}}}}}
 ;  :cljsbuild
 ;  {:builds
